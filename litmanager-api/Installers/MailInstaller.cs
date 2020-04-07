c0b868631241b0d3e72bee0293aaae076525bc27
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-04-07 21:34:26 +0200

    Added Mailing

---
+++ litmanager-api/Installers/MailInstaller.cs
@@ -0,0 +1,22 @@
+﻿using litmanager_api.Mail;
+using litmanager_api.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace litmanager_api.Installers
+{
+    public class MailInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            var emailConfig = Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+
+            services.AddScoped<IMailSender, MailSender>();
+        }
+    }
+}
