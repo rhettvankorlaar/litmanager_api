0d76c003cc8667cf82fbdfa6809a00d5e03ae5d0
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-04-07 11:56:15 +0200

    Added specific mapping per object

--- litmanager-api/Mapping/RegistrationMapping.cs
+++ litmanager-api/Mapping/RegistrationMapping.cs
@@ -4,11 +4,15 @@ using litmanager_api.Contracts.V1.Responses.Registration;
 using litmanager_api.Domain;
 
 namespace litmanager_api.Mapping
+
 {
-    public class EntityToResponse : Profile
+    public class RegistrationMapping : Profile
     {
-        public EntityToResponse()
+        public RegistrationMapping()
         {
+            CreateMap<CreateRequest, Registration>();
+            CreateMap<UpdateRequest, Registration>();
+
             CreateMap<Registration, GetResponse>();
             CreateMap<Registration, User>();
         }
