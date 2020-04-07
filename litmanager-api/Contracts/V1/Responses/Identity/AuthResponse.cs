048e302866c47d90a267bdcb7db4715f9e6ff88b
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-04-07 19:05:41 +0200

    Request and response update

---
+++ litmanager-api/Contracts/V1/Responses/Identity/AuthResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace litmanager_api.Contracts.V1.Responses.Identity
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public bool IsSuccess { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
